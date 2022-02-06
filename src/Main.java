21fbbb6775cbaf3bcd9390c35eb74b66ad4347c3
kate <vuk_kat@mail.ru>
2022-02-06 21:34:10 +0500

    add calculation of bonus miles

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+//        int ticketPriceInKopecks = -1_000_00;
+        int ticketPriceInKopecks = 2_000_00;
+        int costOf1MileInKopecks = 20_00;
+        int numberOfMiles;
 
+        if (ticketPriceInKopecks >= 0) {
+            numberOfMiles = ticketPriceInKopecks / costOf1MileInKopecks;
+        } else {
+            numberOfMiles = 0;
+        }
+
+        System.out.println("Количество начисленных миль за купленный билет: " + numberOfMiles);
     }
 }
